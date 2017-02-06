6ac80fab068e3e83048d08fb09eaee6ef3c9970c
Shane Cunningham <mayhem2277@gmail.com>
2017-02-06 14:57:41 +0000

    added logic for delete function

--- src/calculator/FXMLDocumentController.java
+++ src/calculator/FXMLDocumentController.java
@@ -177,6 +177,10 @@ public class FXMLDocumentController implements Initializable {
             data = Double.parseDouble(display.getText());
             double answer = 1 / data;
             display.setText(String.valueOf(answer));
+
+        } else if (event.getSource() == delete) {
+            data = Double.parseDouble(display.getText());           
+            display.setText(String.valueOf(data).substring(0, display.getLength() - 1));
         } else if (event.getSource() == equalButton) {
 
             double secondOperand = Double.parseDouble(display.getText());
